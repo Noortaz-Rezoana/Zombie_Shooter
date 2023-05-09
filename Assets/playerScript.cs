03d8f0efe6c7cfffb0699530eaf1f4adf705a9b3
Noortaz-Rezoana <noortaz6@gmail.com>
2023-05-09 14:53:07 +0600

    new changes

---
+++ Assets/playerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    CharacterController character;
+    public Transform cameraTransform;
+    bool moving = false;
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraTransform.eulerAngles.x > 35.0f && cameraTransform.eulerAngles.x < 90.0f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+        if (moving)
+        {
+            Vector3 CameraOrientation = cameraTransform.TransformDirection(Vector3.forward);
+            character.SimpleMove(CameraOrientation*speed);
+        }
+    }
+}
